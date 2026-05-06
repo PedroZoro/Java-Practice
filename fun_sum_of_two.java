c3b7ced299a1b3d725da99e2b1b5395f9c493633
PedroZoro <aditikhushi70@gmail.com>
2026-05-06 16:08:02 +0530

    new codes

--- fun_sum_of_two.java
+++ fun_sum_of_two.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class fun_sum_of_two {
     public static void Sum(int num){
-        System.out.println(num);
+        System.out.println(num); // arg of the function
         return;
     }
     public static void main(String[] args) {
@@ -10,6 +10,6 @@ public class fun_sum_of_two {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
-        Sum(num1+num2);
+        Sum(num1+num2); //the operation to be perfomed to give the arg of function value
     }
 }
