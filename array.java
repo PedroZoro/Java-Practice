4231da5ebfc53f88d4ad3c5c0c38d220120da21d
PedroZoro <aditikhushi70@gmail.com>
2026-05-10 14:46:27 +0530

    pushing codes

--- array.java
+++ array.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.NextInt();
-        
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+
+        //for input
+        for(int i=0; i<size; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        //for output
+        for(int i=0; i<size; i++){
+            System.out.println(nums[i]);
+        }
     }
 }
